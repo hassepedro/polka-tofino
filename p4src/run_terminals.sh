8c0cff2905f8e9c7fd6bd65f3b04db9cf1077df4
rafaelsilvag <rafael@infomania.com.br>
2020-09-11 17:45:26 +0000

    add custom port setting for tofino model

--- p4src/run_terminals.sh
+++ p4src/run_terminals.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo tmux -C &
-tmux neww -d './run_tofino_model.sh -p polka; tmux wait -S model' &
+tmux neww -d './run_tofino_model.sh -p polka -f ./ports.json; tmux wait -S model' &
 tmux neww -d './run_switchd.sh -p polka; tmux wait -S switchd' &
