2c7d39211ff9847d84e60e08a058c3293a4f3789
rafaelsilvag <rafael@infomania.com.br>
2020-09-17 22:57:46 -0300

    add suport for full RARE topology

--- p4src/run_terminals.sh
+++ p4src/run_terminals.sh
@@ -2,6 +2,11 @@
 
 tmux -C &
 sleep 2
-tmux neww -d './run_tofino_model.sh -p polka -f ./ports.json; tmux wait -S model' &
+if [ $(hostname) == "frankfurt" ];then
+    tmux neww -d './run_tofino_model.sh -p polka -f ./ports2.json; tmux wait -S model' &
+elif [ $(hostname) == "amsterdam" ];then
+    tmux neww -d './run_tofino_model.sh -p polka -f ./ports2.json; tmux wait -S model' &
+else
+    tmux neww -d './run_tofino_model.sh -p polka -f ./ports.json; tmux wait -S model' &
 sleep 10
 tmux neww -d './run_switchd.sh -p polka; tmux wait -S switchd' &
