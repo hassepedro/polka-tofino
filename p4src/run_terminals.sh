5c370a05c243d7de93126130366375027e7c529f
rafaelsilvag <rafael@infomania.com.br>
2020-09-17 22:58:25 -0300

    bug fix: run_terminals.sh

--- p4src/run_terminals.sh
+++ p4src/run_terminals.sh
@@ -8,5 +8,6 @@ elif [ $(hostname) == "amsterdam" ];then
     tmux neww -d './run_tofino_model.sh -p polka -f ./ports2.json; tmux wait -S model' &
 else
     tmux neww -d './run_tofino_model.sh -p polka -f ./ports.json; tmux wait -S model' &
+fi
 sleep 10
 tmux neww -d './run_switchd.sh -p polka; tmux wait -S switchd' &
