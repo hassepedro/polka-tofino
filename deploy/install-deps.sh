b77780273f8ee7e4f04db5d9953620a4dc371089
rafaelsilvag <rafael@infomania.com.br>
2020-09-20 18:33:58 -0300

    bug fix: avoiding dpkg locking during apt install/update

--- deploy/install-deps.sh
+++ deploy/install-deps.sh
@@ -11,9 +11,6 @@ fi
  
 if [ ! -f ~/runonce ]
 then 
-    #ONCE RUN CODE HERE
-    touch ~/runonce
-
     # Configure the environment
     sudo cat <<EOF > /etc/profile.d/barefoot.sh
 export SDE=/extra/bf-sde-9.2.0
@@ -26,12 +23,18 @@ EOF
     tar xvf ~/p4studio/bf-sde-9.2.0.tar -C /extra/
     tar xvf ~/p4studio/ba-tools.tar -C /extra/
 
+    # Check and wait until dpkg system to be unlocked
+    while sudo fuser /var/{lib/{dpkg,apt/lists},cache/apt/archives}/lock >/dev/null 2>&1; do
+        sleep 1
+    done
     # Install based on switch_p4_16 profile 
     cd $SDE/p4studio_build && ./p4studio_build.py --use-profile switch_p416_profile
-
+    
     # Creating virtual interfaces
     /extra/tools/veth_setup.sh
 
+    # ONCE RUN CODE HERE
+    touch ~/runonce
     # Install requirements 
     # sudo apt install iperf3 jq -y
 fi
